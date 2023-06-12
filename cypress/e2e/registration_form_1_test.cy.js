62b53a2469cbbdac55e0fe692d4f4933ea5c2541
Andres-Ra <Andres.Raev@gmail.com>
2023-06-12 17:37:00 +0300

    Muudatused 17:37

--- cypress/e2e/registration_form_1_test.cy.js
+++ cypress/e2e/registration_form_1_test.cy.js
@@ -17,7 +17,7 @@ Assignment 2:
  */
 
 describe('This is first test suite, Andres Raev', () => {
-    //Todays date: 12.06.2023-1
+    //Todays date: 12.06.2023
     it('User can submit data only when valid mandatory values are added', () => {
         cy.get('[data-testid="phoneNumberTestId"]').type('555666777')
         cy.get('input[name="password"]').type('MinuParool')
